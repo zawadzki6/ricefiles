fbfa9cded303a7b46d831315eb983750add936f6
Zawadzki <134331872+zawadzki6@users.noreply.github.com>
2025-09-09 08:06:34 +0200

    Add files via upload

--- rice/fixes.sh
+++ rice/fixes.sh
@@ -4,6 +4,7 @@ xrandr --output DisplayPort-1 --mode 2560x1440
 
 input-remapper-control --command autoload
 pkill -f "bash /home/zawadzki/rice/listener.sh"
+pkill -f "python /home/zawadzki/rice/autotile.py"
 sleep 1s
 
 for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; done
@@ -13,3 +14,6 @@ xset s off -dpms s noblank
 xset r rate 400 50
 
 bash ~/rice/listener.sh &
+python /home/zawadzki/rice/autotile.py &
+
+bash ~/rice/xrandr.sh &
--- rice/power/reboot.sh
+++ rice/power/reboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# paplay /home/zawadzki/rice/sounds/desktop-logout.ogg
+paplay /home/zawadzki/rice/sounds/Windows_Logoff_Sound.wav
 systemctl reboot
