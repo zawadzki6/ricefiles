99030d01f95c605277dba9edd4c8c085465a6f84
Zawadzki <zawadzki0@proton.me>
2025-12-08 23:49:20 +0100

    Add files via upload

--- rice/power.sh
+++ rice/power.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-options="⏻ Shutdown\n  Reboot\n  Logout\n󰤄 Suspend\n  Lock"
+options=" 󰤆   Shutdown\n    Reboot\n    Logout\n 󰤄   Suspend\n 󰑹   Hibernate\n    Lock"
 
 chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power" -theme ~/rice/rofi.rasi)
 
 case "$chosen" in
-    "⏻ Shutdown")  bash ~/rice/power/shutdown.sh ;;
+    "󰤆 Shutdown")  bash ~/rice/power/shutdown.sh ;;
     "  Reboot")    bash ~/rice/power/reboot.sh ;;
     "  Logout")    bash ~/rice/power/logout.sh ;;
     "󰤄 Suspend")   bash ~/rice/lock.sh && systemctl suspend ;;
+    "󰑹 Hibernate") bash ~/rice/power/hibernate.sh ;;
     "  Lock")      bash ~/rice/lock.sh ;;
     *)             exit 1 ;;
 esac
